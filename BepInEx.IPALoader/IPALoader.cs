882ba4097f530fab879bc01b1b21abc3803b17a8
ghorsington <neigh@coder.horse>
2019-08-22 23:08:46 +0300

    Disable loader if original IPA is detected

--- BepInEx.IPALoader/IPALoader.cs
+++ BepInEx.IPALoader/IPALoader.cs
@@ -9,14 +9,27 @@ namespace BepInEx.IPALoader
 	public class IPALoader : BaseUnityPlugin
 	{
 		internal new static ManualLogSource Logger;
+		private bool run = true;
 
 		private IPALoader()
 		{
 			Logger = base.Logger;
 
+			foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+			{
+				var name = assembly.GetName().Name;
+				if (name == "IllusionInjector" || name == "IllusionPlugin")
+				{
+					Logger.LogWarning($"Detected that IPA is already installed! Please either uninstall IPA or disable this loader.");
+					run = false;
+					return;
+				}
+			}
+
 			// Redirect missing assembly requests to this assembly, since we have all the emulated code
 			AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
 			{
+				Logger.LogInfo(args.Name);
 				var name = new AssemblyName(args.Name).Name;
 				if (name == "IllusionPlugin" ||
 					name == "IllusionInjector")
@@ -28,7 +41,8 @@ namespace BepInEx.IPALoader
 
 		private void Awake()
 		{
-			Injector.Inject();
+			if(run)
+				Injector.Inject();
 		}
 	}
 }
