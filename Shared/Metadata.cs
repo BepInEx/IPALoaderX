739a7e5ba5e4210d06abf87c16cc5b65d39d3785
ghorsington <neigh@coder.horse>
2020-01-11 21:43:40 +0200

    Bump version

--- Shared/Metadata.cs
+++ Shared/Metadata.cs
@@ -2,7 +2,7 @@
 {
     public static class Metadata
     {
-        public const string PluginsVersion = "1.2.1";
+        public const string PluginsVersion = "1.2.2";
 
         internal const string ConfigFileName = "BepInEx.IPALoader.cfg";
         internal const string ConfigSection = "Config";
